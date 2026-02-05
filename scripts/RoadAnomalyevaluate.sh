8712826be88b935769d95c5ef8b479ff66f7eedd
Hu Tianshun <121536066+gitfrahts@users.noreply.github.com>
2026-02-05 15:39:29 +0800

    Add files via upload

--- scripts/RoadAnomalyevaluate.sh
+++ scripts/RoadAnomalyevaluate.sh
@@ -1,14 +1,29 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES=0,2,3
-NGPU=3
-
+export CUDA_VISIBLE_DEVICES=4,6
+NGPU=2
 
 # evaluate results on RoadAnomaly
-torchrun --nproc_per_node=$NGPU --master_port=12345 evaluate.py \
+torchrun --nproc_per_node=$NGPU --master_port=29500 evaluate.py \
     --score_mode bsl \
-    --snapshot ./pretrained/ra.pth \
+    --snapshot pretrained/ra.pth \
     --inference_scale 0.5 0.65 0.85 1.0 1.25 1.75 \
     --inf_temp 1.0 \
-    --anomaly_dataset ra
+    --anomaly_dataset ra \
+    --visualize \
+    --vis_num 50 \
+    --vis_threshold 0.5
 
+# #!/usr/bin/env bash
+
+# export CUDA_VISIBLE_DEVICES=3,5
+# NGPU=2
+
+
+# # evaluate results on RoadAnomaly
+# torchrun --nproc_per_node=$NGPU --master_port=29500 evaluate.py \
+#     --score_mode bsl \
+#     --snapshot pretrained/ra.pth \
+#     --inference_scale 0.5 0.65 0.85 1.0 1.25 1.75 \
+#     --inf_temp 1.0 \
+#     --anomaly_dataset ra
